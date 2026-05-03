8f35526fc323cc27aa32067aeb1c3f44ab3f84e8
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-05-03 15:47:26 -0700

    Day 3 WIP: Streak card with flame, number, label, and calendar dots

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,16 @@
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
 
 export default function HomeScreen() {
+  const weekData = [
+    { day: 'M', isComplete: true },
+    { day: 'T', isComplete: true },
+    { day: 'W', isComplete: true },
+    { day: 'T', isComplete: true },
+    { day: 'F', isComplete: false },
+    { day: 'S', isComplete: false },
+    { day: 'S', isComplete: false },
+  ];
   return (
     <ScrollView 
       style={styles.container}
@@ -8,6 +18,24 @@ export default function HomeScreen() {
     >
       <Text style ={styles.greeting}>Good Evening,</Text>
       <Text style ={styles.name}>Olisaemeka</Text>
+
+      <View style={styles.streakCard}>
+        <Text style={styles.flameIcon}>🔥</Text>
+        <Text style={styles.streakNumber}>47</Text>
+        <Text style={styles.streakLabel}>day streak</Text>
+      </View>
+
+       <View style={styles.calendarRow}>
+          {weekData.map((item, index) => (
+            <View 
+              key={index}
+              style={[
+                styles.calendarDot,
+                item.isComplete ? styles.dotComplete : styles.dotIncomplete
+              ]}
+            />
+          ))}
+      </View>
     </ScrollView>
   );
 }
@@ -31,8 +59,62 @@ const styles = StyleSheet.create({
   name: {
     color: "#FFFFFF",
     fontSize: 24,
-    marginBottom: 24,
+    marginBottom: 40,
+  }, 
+
+  streakCard: {
+    marginTop: 16,
+    alignItems: 'center',
+    padding: 24,
+    marginBottom: 16,
+  },
+
+
+  flameIcon: {
+    fontSize: 30,
+    marginBottom: 0,
+    marginLeft: 5,
+    lineHeight:30,
+  },
+
+  streakNumber: {
+    color: "#FF6B35",
+    fontSize: 140,
+    marginTop: 5,
+    lineHeight: 140,
+  },
+
+  streakLabel: {
+  color: "#9E9E9E",
+  fontSize: 14,
+  lineHeight: 14,
+  marginTop: -10,  // Negative margin pulls it closer to the number above
+  marginBottom: 4,
+},
+
+  calendarRow: {
+    flexDirection: 'row',
+    gap: 10, 
+  },
+
+  dotIncomplete: {
+    backgroundColor: '#FF6B35',
+    borderColor: '#FF6B35',
+  },
+
+  dotComplete: {
+  backgroundColor: 'transparent',
+  borderColor: '#6B6B6B',
+  },
+
+  calendarDot: {
+    width: 32,
+    height: 32,
+    borderRadius: 16,
+    borderWidth: 2,
   }
+
+
 
 
 });
