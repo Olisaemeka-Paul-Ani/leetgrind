2f365429368f2a39fcd35c421e45f45288bbaafd
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-05-10 01:19:29 -0700

    Feat: add Streak loss risk card

--- app/(tabs)/group.tsx
+++ app/(tabs)/group.tsx
@@ -11,6 +11,14 @@ export default function GroupScreen() {
           <MaterialIcons name="content-copy" size={12} color= "#FF6B35" />
         </TouchableOpacity>
       </View>
+      <View style = {{justifyContent: 'center', alignItems: 'center', marginTop: 30, gap:10, flexDirection: 'column',marginBottom:0,}}>
+       <Text style = {styles.groupStreak}>12</Text>
+       <Text style = {{color: "#9E9E9E", fontSize:14,}}>GROUP STREAK</Text>
+       <View style = {styles.groupStats}>
+        <View style = {{height: 5, width: 5, borderRadius: 5, backgroundColor:'#22C55E'}}></View>
+        <Text style = {{color:"#DC2626", fontSize:11}}>STREAK AT RISk • 3 PENDING</Text>
+       </View>
+      </View>
     </ScrollView>
   );
 }
@@ -33,7 +41,7 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingRight: 10,
     height:20,
-    width: 70,
+    width: 80,
     borderRadius: 10,
     flexDirection : 'row',
     gap: 5
@@ -45,4 +53,28 @@ const styles = StyleSheet.create({
     padding: 16,
     paddingTop:50,
   },
+
+  groupStreak: {
+    color: "#FF6B35",
+    fontSize: 110,
+    marginTop: 5,
+    lineHeight: 125,
+    marginBottom: 0,
+    includeFontPadding: false,
+    textAlignVertical: 'center',
+  },
+
+  groupStats :{
+    backgroundColor: '#1C1C1C',
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 10,
+    height:30,
+    width: 210,
+    borderRadius: 10,
+    flexDirection : 'row',
+    gap: 5,
+    marginTop: 20,
+    borderWidth:1,
+  }
 });
